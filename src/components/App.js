8a433a0752673dfecff310b83890fe8e2af5e9b2
Ashna Malhotra <ashna.malhotra@appirio.com>
2020-04-15 00:10:24 +0530

    Show Video list along with iframe to show selected video

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube, { baseParams } from '../apis/youtube';
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onSearchTermSubmit('cars');
+  }
+
   onSearchTermSubmit = async (term) => {
-    console.log(term);
     const response = await youtube.get('/search', {
       params: {
         ...baseParams,
         q: term,
       },
     });
-    console.log(response);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({
+      selectedVideo: video,
+    })
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSearchTermSubmit={this.onSearchTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
